5ee8d314fefee43c6ae332cfa65bdb8843165b00
johannes <johannes.lewen@lewen.eu>
2025-04-22 10:49:58 +0200

    update the animals_template.html with data from animals-data.json

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -19,6 +19,39 @@ def print_data(animals):
         print()
 
 
-if __name__ == '__main__':
+def generate_animals_string(animals_data: list) -> str:
+    output_str = ""
+    for animal in animals_data:
+        output_str += f"Name: {animal.get('name')}\n"
+        if animal.get('characteristics').get('diet'):
+            output_str += f"Diet: {animal.get('characteristics').get('diet')}\n"
+        if animal.get('locations'):
+            output_str += f"Location: {animal.get('locations')[0]}\n"
+        if animal.get('characteristics').get('type'):
+            output_str += f"Type: {animal.get('characteristics').get('type')}\n"
+        output_str += '\n'
+    return output_str
+
+
+def update_html(filepath: str,
+                str_pattern: str = '__REPLACE_ANIMALS_INFO__',
+                str_input: str = '',):
+    """Read html file at the provided filepath
+    and update the given pattern with new string input"""
+    with open(filepath, 'r') as html_file:
+        html_content = html_file.read()
+    html_content = html_content.replace(str_pattern, str_input)
+    with open('animals.html', 'w') as html_file:
+        html_file.write(html_content)
+    return html_content
+
+
+def main():
     animal_data = load_json('animals-data.json')
     print_data(animal_data)
+
+
+if __name__ == '__main__':
+    animal_data = load_json('animals-data.json')
+    animal_str = generate_animals_string(animal_data)
+    print(update_html(filepath='animals_template.html', str_input=animal_str))
